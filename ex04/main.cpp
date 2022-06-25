ede557fe2a6916774f08535e328627314c5af080
Konstantin Meixner <konstantin.meixner@freenet.de>
2022-06-25 18:33:04 +0200

    ex04

---
+++ ex04/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmeixner <konstantin.meixner@freenet.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/09 19:00:17 by kmeixner          #+#    #+#             */
+/*   Updated: 2022/06/25 18:10:25 by kmeixner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int	main(int argc, char *argv[]) 
+{
+	if (argc != 4 || !argv[2][0] || !argv[3][0])
+		return (0);
+	std::ifstream	ifs(argv[1]);
+	std::string 	content;
+	std::string 	buffer;
+	while(std::getline(ifs, buffer))
+		content.append(buffer + "\n");
+	ifs.close();
+	size_t	pos = content.find(argv[2]);
+	while (pos != std::string::npos)
+	{
+		content.erase(pos, strlen(argv[2]));
+		content.insert(pos, argv[3]);
+		pos = content.find(argv[2]);
+	}
+	std::string	ofile = argv[1];
+	ofile.append(".replace");
+	std::ofstream	ofs(ofile.c_str());
+	ofs << content;
+	ofs.close();
+}
