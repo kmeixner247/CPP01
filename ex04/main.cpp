bdd41e24a133c1b2422423d9ad6310d4eeddc6c0
Konstantin Meixner <konstantin.meixner@freenet.de>
2022-06-25 19:07:15 +0200

    no idea

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kmeixner <konstantin.meixner@freenet.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 19:00:17 by kmeixner          #+#    #+#             */
-/*   Updated: 2022/06/25 18:10:25 by kmeixner         ###   ########.fr       */
+/*   Updated: 2022/06/25 18:58:00 by kmeixner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <fstream>
 #include <string>
 
-int	main(int argc, char *argv[]) 
+int	main(int argc, char *argv[])
 {
 	if (argc != 4 || !argv[2][0] || !argv[3][0])
 		return (0);
